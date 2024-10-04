7f607f5a249e6264fbcd4335b50c8ae7a78aebbd
local user <user@mil8a-102a-10.mmcs.sfedu.ru>
2024-10-04 13:27:07 +0300

    add day

--- time.pas
+++ time.pas
@@ -4,9 +4,9 @@
 begin
   var n := ReadInteger('Укажите год');
   if ((n mod 4 = 0 ) and (n mod 100 <> 0)) or (( n mod 400 <> 0 ) and (n mod 100 = 0)) then
-    Print('Високосный')
+    Print('366 дней')
   else
-    Print('Не является високосным');
+    Print('365 дней');
 end.
 
 {Укажите год 2004 Високосный }
