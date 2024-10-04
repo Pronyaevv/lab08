6eb6c6a87a0c37d1896cf477d259e38f42fd8cd8
local user <user@mil8a-102a-10.mmcs.sfedu.ru>
2024-10-04 13:09:16 +0300

    Add time

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  Print('Привет,мир');
+  var time := ReadInteger('Который час?');
+  Assert(time in 0..23);
+  case time of
+    4..10: writeln('Доброе утро,мир!');
+    11..16: writeln('Добрый день,мир!');
+    17..22: writeln('Добрый вечер,мир!');
+    else writeln('Доброй ночи, мир!');
+    
+  end;
 end.
